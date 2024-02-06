dd89c457a2365bc0a3f68cc9d914550a8422330b
Veselina S <vesi.as@abv.bg>
2024-02-06 19:34:18 +0200

    update templates

--- Petstagram_project/common/views.py
+++ Petstagram_project/common/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from Petstagram_project.photos.models import PhotoPet
+
 
 def home_page(request):
-    context = {}
+    context = {
+        "photos_of_pet": PhotoPet.objects.all(),
+    }
 
     return render(request, "common/home-page.html", context)
